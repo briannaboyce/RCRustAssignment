7c1eda27890b1a4f0785f29c99a9b912923662d7
Brianna Boyce <brieboyce@gmail.com>
2021-02-08 16:40:09 -0500

    Iterate over command list in txt file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,12 @@
 use std::env;
-use std::io::{stdin, stdout, Write};
 use std::process::Command;
 use std::fs;
 
 
 fn main() {
-	//println!("Hello World!");
-	/*First collect input from a human user
-		later we'll get the input from a file
-		when the program starts, it should start with an argument to the file of commands
-	*/
+
+	//Collect input from a file
+	//Should limit file type to something specific like yaml, using txt for now
 	let args: Vec<String> = env::args().collect();
 	let mut commands: Vec<String> = Vec::new();
 
@@ -24,25 +21,27 @@ fn main() {
 	}
 	
 
-//should iterate over commands to provide list of commands
-	let command = &commands[0];
-	println!("{}", command);
+	//Iterate over list of commands
+	for command in commands {
+		let process = if cfg!(target_os = "windows") {
 
-	let process = if cfg!(target_os = "windows") {
-		Command::new("cmd")
-				.args(&["/C", command])
-				.output()
-				.expect("failed to execute")
-	} else {
-		Command::new("sh")
-				.arg(command)
-				.output()
-				.expect("failed to execute") 
-	};
+			Command::new("cmd")
+					.args(&["/C", &command])
+					.output()
+					.expect("failed to execute")
+		} else {
+			Command::new("sh")
+					.arg("-c")
+					.arg(&command)
+					.output()
+					.expect("failed to execute") 
+		};
 
-	let hello = std::string::String::from_utf8(process.stdout)
-									.ok()
-									.expect("failure");
-	print!{"{}", hello};
+		let output = std::string::String::from_utf8(process.stdout)
+										.ok()
+										.expect("failure");
+
+		println!("{}", output);
+	}
 
 }
