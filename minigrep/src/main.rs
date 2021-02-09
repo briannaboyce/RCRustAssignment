a47898dd04df04d69c8f8acdfe98e485b68c53ca
Brianna Boyce <brieboyce@gmail.com>
2021-02-09 13:08:33 -0500

    Use yaml test file and create structs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,31 @@
 use std::env;
-use std::process::Command;
 use std::fs;
+use std::process::Command;
+use serde_yaml;
+use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+struct Process {
+	path: String,
+	name: String,
+	arguments: std::collections::HashMap<String, String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+struct File {
+	path: String,
+	r#type: String,
+	name: String,
+	content: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+struct InputFile {
+	process: std::collections::HashMap<String, Process>,
+	create: std::collections::HashMap<String, File>,
+	update: std::collections::HashMap<String, File>,
+	delete: std::collections::HashMap<String, File>,
+}
 
 fn main() {
 
@@ -11,11 +35,17 @@ fn main() {
 	let mut commands: Vec<String> = Vec::new();
 
 	let file = &args[1];
-
 	let contents = fs::read_to_string(file)
 		.expect("File does not exist");
 
-	for line in contents.lines() {
+	println!("{}", contents);
+
+	let yaml_data = serde_yaml::from_str::<InputFile>(&contents);
+	yaml_data.process[0];
+
+	println!("{:?}", yaml_data);
+
+	/*for line in contents.lines() {
 		//need to check if valid command
 		commands.push(line.to_string());
 	}
@@ -23,29 +53,23 @@ fn main() {
 
 	//Iterate over list of commands
 	for command in commands {
-		let process = if cfg!(target_os = "windows") {
+		//let process = 
+		if cfg!(target_os = "windows") {
 
 			Command::new("cmd")
 					.args(&["/C", &command])
-					.output()
+					.spawn()
 					.expect("failed to execute")
 		} else {
 			Command::new("sh")
 					.arg("-c")
 					.arg(&command)
-					.output()
+					.spawn()
 					.expect("failed to execute") 
 		};
 
-		let output = std::string::String::from_utf8(process.stdout)
-										.ok()
-										.expect("failure");
-
-		//println!("process ID is {}", process::id());
-
-		print!("{}", output);
 		println!(".");
-	}
+	}*/
 		println!("All done");
 
 }
